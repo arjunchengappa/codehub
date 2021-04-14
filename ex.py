9ab9369905db461ba68c44844c979ba271db9515
arjunchengappa <65189062+arjunchengappa@users.noreply.github.com>
2021-04-14 22:23:50 +0530

    Add files via upload
    delete database and root directory contents when its run. Basically resets the app

--- ex.py
+++ ex.py
@@ -1,124 +1,14 @@
-'''
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        try:
-            if session['user']:
-                return redirect(url_for('index'))
-        except:
-            return render_template('login.html', data=None)
-    if request.method == 'POST':
-        if request.form['email'] and request.form['password']:
-            email = request.form['email']
-            password = request.form['password']
-            conn = sql.connect('scamhub.db')
-            cur = conn.cursor()
-            cur.execute('select username, password from users where email = ?', (email,))
-            row = cur.fetchall()
-            if len(row) == 1:
-                dcy_password = cipher_suite.decrypt(row[0][1])
-                if dcy_password.decode() == password:
-                    session['user'] = row[0][0]
-                    return redirect(url_for('index'))
-            data = dict()
-            data['message'] = 'Incorrect Email/Password'
-            return render_template('login.html', data=data)
-        return render_template('login.html', data=None)
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    try:
-        session.pop('user', None)
-    except:
-        pass
-    return redirect(url_for('index'))
-
-@app.route('/createRepository', methods=['GET', 'POST'])
-def create():
-    if request.method == 'GET':
-        try:
-            if session['user']:
-                return render_template('createRepo1.html', data=None)
-            return redirect(url_for('index'))
-        except:
-            return redirect(url_for('index'))
-    if request.method == 'POST':
-        repo_name = request.form['name']
-        description = request.form['description']
-        num_of_contributors = request.form['numContributors']
-        conn = sql.connect('scamhub.db')
-        cur = conn.cursor()
-        cur.execute('select * from repositories where repo_name = ?', (repo_name,))
-        row = cur.fetchall()
-        if len(row) != 0:
-            data = {'message': repo_name, 'description': description}
-            return render_template('createRepo1.html', data=data)
-        print(type(num_of_contributors))
-        if num_of_contributors == '0':
-            contributors_list = "[]"
-            cur.execute('insert into repositories (repo_name, repo_owner, repo_description, contributors_list) values (?, ?, ?, ?)', (repo_name, description, session['user'], contributors_list))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-        else:
-            cur.execute('insert into repositories (repo_name, repo_owner, repo_description) values (?, ?, ?)', (repo_name, description, session['user']))
-            conn.commit()
-            conn.close()
-            session['numContributors'] = num_of_contributors
-            session['repo_name'] = repo_name
-            return redirect(url_for('contributors'))
-
-@app.route('/contributors', methods=['GET', 'POST'])
-def contributors():
-    if request.method == 'GET':
-        try:
-            if session['user']:
-                if session['numContributors']:
-                    data = {'numContributors': int(session['numContributors'])}
-                    return render_template('createRepo2.html', data=data)
-            return redirect(url_for('index'))
-        except:
-            return redirect(url_for('index'))
-    if request.method == 'POST':
-        contributors = list(dict(request.form).values())
-        contributors_list = '[\'' + '\',\''.join(contributors)+'\']'
-        conn = sql.connect('scamhub.db')
-        cur = conn.cursor()
-        for i in range(len(contributors)):
-            cur.execute('select * from users where username = ?', (contributors[i],))
-            row = cur.fetchall()
-            if len(row) != 1:
-                data = {'numContributors': session['numContributors'], 'error' : i, 'error_contributor' : contributors[i]}
-                return render_template('createRepo2.html', data=data)
-        cur.execute('update repositories set contributors_list = ? where repo_name = ?', (contributors_list, session['repo_name']))
-        conn.commit()
-        conn.close()
-        session.pop('repo_name', None)
-        session.pop('numContributors', None)
-        return redirect(url_for('index'))
-
-
-
-@app.route('/repositories')
-def repositories():
-    username = session['user']
-    return 'hi'
-    return render_template('repositories.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'GET':
-        return render_template('upload.html')
-    if request.files['file']:
-        f = request.files['file']
-        f.save('root/' + f.filename)
-        return 'Hello'
-'''
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
 
 engine = create_engine('sqlite:///scamhub.db', echo = True)
 meta = MetaData()
+conn = engine.connect()
+
+command = "DROP TABLE IF EXISTS {};".format('users')
+conn.execute(command)
+command = "DROP TABLE IF EXISTS {};".format('repository_table')
+conn.execute(command)
+conn.close()
 
 users = Table(
    'users', meta,
@@ -142,8 +32,9 @@ repository_table = Table(
 )
 
 meta.create_all(engine)
-conn = engine.connect()
 
-res = conn.execute(users.select())
-for row in res:
-    print(row)
+import os
+import shutil
+dir = os.getcwd() + '/static/root'
+shutil.rmtree(dir)
+os.mkdir(dir)

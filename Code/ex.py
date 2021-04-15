46a1d6e41eea58176475c98f4aa70ca68eca6ca0
arjunchengappa <65189062+arjunchengappa@users.noreply.github.com>
2021-04-16 00:18:21 +0530

    Add files via upload
    minor bug updates

---
+++ Code/ex.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
+
+engine = create_engine('sqlite:///Codehub.db', echo = True)
+meta = MetaData()
+conn = engine.connect()
+
+command = "DROP TABLE IF EXISTS {};".format('users')
+conn.execute(command)
+command = "DROP TABLE IF EXISTS {};".format('repository_table')
+conn.execute(command)
+conn.close()
+
+users = Table(
+   'users', meta,
+    Column('user_id', Integer, primary_key = True),
+    Column('name', String),
+    Column('username', String),
+    Column('email', String),
+    Column('password', String),
+    Column('repos_owned', Integer),
+    sqlite_autoincrement=True
+)
+
+repository_table = Table(
+    'repository_table', meta,
+    Column('repo_id', Integer, primary_key = True),
+    Column('repo_name', String),
+    Column('repo_owner', String),
+    Column('repo_description', String),
+    Column('contributors_list', String),
+    sqlite_autoincrement=True
+)
+
+meta.create_all(engine)
+
+import os
+import shutil
+dir = os.getcwd() + '/static/root'
+shutil.rmtree(dir)
+os.mkdir(dir)
+dir = os.getcwd() + '/static/img/profile'
+shutil.rmtree(dir)
+os.mkdir(dir)
